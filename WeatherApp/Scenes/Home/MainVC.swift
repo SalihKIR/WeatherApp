1c70e810fe2edf96d26ce7bc9dfe972fb0c1f56f
Salih KIR <salihkir2020@gmail.com>
2022-06-25 23:48:34 +0300

    Edited CollectionView

--- WeatherApp/Scenes/Home/MainVC.swift
+++ WeatherApp/Scenes/Home/MainVC.swift
@@ -18,18 +18,44 @@ class MainVC: UIViewController {
         super.viewDidLoad()
         statusCollectionView.delegate = self
         statusCollectionView.dataSource = self
+        statusCollectionView.register(WeatherCollectionCell.self, forCellWithReuseIdentifier: "WeatherCollectionCell")
         
+        let tasarim = UICollectionViewFlowLayout()
+              tasarim.sectionInset = UIEdgeInsets(top: 150, left: 30, bottom: 0, right: 30)
+              tasarim.minimumLineSpacing = 0
+              tasarim.minimumInteritemSpacing = 0
+              
+              let genislik = statusCollectionView.frame.size.width
+        let hucreGenislik = (genislik-100)/1
+        tasarim.itemSize = CGSize(width: hucreGenislik, height: hucreGenislik*1)
+        statusCollectionView!.collectionViewLayout = tasarim
         viewmodel.getUpcomingData()
+    }
+
+    //        AppRouter.shared.showDetailPage(self.navigationController, data: viewmodel.weatherData!)
+    
+}
+    extension MainVC: UICollectionViewDelegate , UICollectionViewDataSource{
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return 1
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let weathercell = statusCollectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCollectionCell", for: indexPath) as! WeatherCollectionCell
+            weathercell.layer.borderColor = UIColor.systemIndigo.cgColor
+            weathercell.layer.borderWidth = 0.6
+            weathercell.layer.cornerRadius = 10
+            
+            return weathercell
+        }
+//        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+//            {
+//                return CGSize(width: 300, height: 300)
+//            }
+       
+      
         
     }
-    
-    //    @IBAction func testBUTTON(_ sender: Any) {
-    //        AppRouter.shared.showDetailPage(self.navigationController, data: viewmodel.weatherData!)
-    //
-    //    }
-}
-
-
 extension MainVC: MainVMDelegateOutputs {
     func successHeader(_ type: MainVMOutputs) {
         switch type {
@@ -40,24 +66,5 @@ extension MainVC: MainVMDelegateOutputs {
         }
     }
 }
-    extension MainVC: UICollectionViewDelegate , UICollectionViewDataSource{
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-           return 5
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let weathercell = statusCollectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCollectionCell", for: indexPath)
-           // weathercell.layer.borderColor = UIColor.systemIndigo.cgColor
-//            weathercell.layer.borderWidth = 0.3
-//            weathercell.layer.cornerRadius = 10
-            
-            return weathercell
-        }
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
-            {
-                return CGSize(width: 100, height: 100)
-            }
-        
-    }
     
 
